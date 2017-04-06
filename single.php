a943b085ce4b0601353b2a79039cd4358c8f017f
Tania Rascia <taniarascia@gmail.com>
2017-04-06 15:24:59 -0500

    Simplify

--- single.php
+++ single.php
@@ -1,9 +1,9 @@
-<?php get_header(); ?>
+<?php get_header(); 
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
-<?php get_template_part( 'content-single', get_post_format() ); ?>
+	get_template_part( 'content-single', get_post_format() ); 
 
-<?php endwhile; endif; ?>
+endwhile; endif; 
 
-<?php get_footer(); ?>
+get_footer(); 
