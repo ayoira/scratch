786bb167b0186f488ed1fbd49635870bae5bf24e
Tania Rascia <taniarascia@gmail.com>
2017-04-18 10:04:33 -0500

    Update home_url

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-	<title>Scratch Theme</title>
+	<title><?php echo get_bloginfo( 'name' ); ?></title>
 	
 	<link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' ); ?>/css/main.css">
 </head>
@@ -11,7 +11,7 @@
 	<header>
 		<h1><?php echo get_bloginfo( 'name' ); ?></h1>
 		<nav>
-			<a href="<?php echo site_url(); ?>">Blog</a>
-			<a href="<?php echo site_url(); ?>/sample-page">Sample Page</a>
+			<a href="<?php echo home_url(); ?>">Blog</a>
+			<a href="<?php echo home_url(); ?>/sample-page">Sample Page</a>
 		</nav>
 	</header>
